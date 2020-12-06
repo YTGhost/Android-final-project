687fc8209ed44c428846068a176b1a6d7392dbda
YTGhost <283304489@qq.com>
2020-12-07 00:27:34 +0800

    feat: 发送邮件和jedis配置完毕

---
+++ android-back-end/src/main/java/com/bjtu/androidbackend/util/MailService.java
@@ -0,0 +1,26 @@
+package com.bjtu.androidbackend.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        message.setFrom(from);
+        mailSender.send(message);
+    }
+}
