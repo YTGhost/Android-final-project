04db8c905066c81d66b70e8994abddf2354fbc11
CodingLiOOT <296277916@qq.com>
2020-12-10 16:37:14 +0800

    添加 build文件夹

---
+++ CampusInformationPlatform/lib-todaystepcounter/build/generated/source/buildConfig/debug/com/today/step/lib/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.today.step.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.today.step.lib";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final boolean TODAY_STEP_DEBUG = true;
+}
