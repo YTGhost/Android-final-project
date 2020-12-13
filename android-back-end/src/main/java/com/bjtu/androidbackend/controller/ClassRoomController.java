2ba68f6324d2d2157db26f97018111c7f7e236d7
YTGhost <283304489@qq.com>
2020-12-13 23:58:20 +0800

    feat: 空闲教室数据

--- android-back-end/src/main/java/com/bjtu/androidbackend/controller/ClassRoomController.java
+++ android-back-end/src/main/java/com/bjtu/androidbackend/controller/ClassRoomController.java
@@ -22,9 +22,32 @@ public class ClassRoomController {
     public Map<String, Object> getFreeroom(@RequestBody ClassRoom classRoom) {
         Map<String, Object> map = new HashMap<>();
         List<String> list = new ArrayList<>();
-        list.add("301");
-        list.add("310");
-        list.add("204");
+        String building = classRoom.getBuilding();
+        if(building.equals("逸夫")) {
+            list.add("301");
+            list.add("302");
+            list.add("304");
+            list.add("310");
+            list.add("402");
+            list.add("405");
+            list.add("507");
+            list.add("510");
+        } else if (building.equals("思西")){
+            list.add("102");
+            list.add("101");
+            list.add("204");
+            list.add("205");
+            list.add("301");
+            list.add("303");
+            list.add("304");
+            list.add("306");
+        } else if (building.equals("思源")) {
+            list.add("107");
+            list.add("108");
+        } else {
+            list.add("202");
+            list.add("204");
+        }
         map.put("code", 0);
         map.put("data", list);
         map.put("msg", "");
